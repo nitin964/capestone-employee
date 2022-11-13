6e0ae7d72928c9e4dd620890959a42b421c52e84
nitin964 <110768071+nitin964@users.noreply.github.com>
2022-11-13 14:16:08 +0530

    Add files via upload

--- starter/setup.sh
+++ starter/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export DATABASE_URL="postgresql://postgres:admin@localhost:5432/postgres"
-export EXCITED="true"
+export AUTH0_DOMAIN="dev-qhqw-viy.us.auth0.com"
+export ALGORITHMS="['RS256']"
+export API_AUDIENCE="employee"
 echo "setup.sh script executed successfully!"

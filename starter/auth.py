9542749f9bc9b7a3bf3cba40e60abfcbd05826aa
nitin964 <110768071+nitin964@users.noreply.github.com>
2022-11-13 16:25:00 +0530

    Add files via upload

--- starter/auth.py
+++ starter/auth.py
@@ -124,10 +124,9 @@ def requires_auth(permission=''):
             try:
                 token = get_token_auth_header()
                 payload = verify_decode_jwt(token)
+                check_permissions(permission, payload)
+                return f(payload, *args, **kwargs)
             except AuthError as e:
                 abort(e.status_code)
-
-            check_permissions(permission, payload)
-            return f(payload, *args, **kwargs)
         return wrapper
     return requires_auth_decorator

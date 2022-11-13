6eb1162842ad213a09183a41fa69c5bf36272e3b
nitin964 <110768071+nitin964@users.noreply.github.com>
2022-11-13 14:24:25 +0530

    Update setup.sh

--- starter/setup.sh
+++ starter/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:admin@localhost:5432/postgres"
+export DATABASE_URL="postgres://wnkkffkrvlpbsj:f9166de39b032c1ec9df5c9d1c59bb46bcfe5b02d2b642ebb287e3148bceae4f@ec2-44-209-186-51.compute-1.amazonaws.com:5432/dbg9a7hhai2665"
 export AUTH0_DOMAIN="dev-qhqw-viy.us.auth0.com"
 export ALGORITHMS="['RS256']"
 export API_AUDIENCE="employee"
